cf83c42edef5f9132f0bf48cc3684ccde21a23af
Chris Mague <cmague@machinezone.com>
2014-04-30 12:15:12 -0700

    add in rvm and java to build various jruby stuff

--- puppet/modules/packagebuild/manifests/init.pp
+++ puppet/modules/packagebuild/manifests/init.pp
@@ -1,7 +1,7 @@
 # install and configure fpm for the building of RPMS
 class packagebuild {
 
-  $ruby_pkgs = [ 'ruby1.9.1-dev', 'ruby1.9.1', 'rubygems' ]
+  $ruby_pkgs = [ 'ruby1.9.1-dev', 'ruby1.9.1', 'rubygems', 'ruby-rvm' ]
 
   package { $ruby_pkgs:
     ensure => latest,
@@ -13,11 +13,24 @@ class packagebuild {
     require  => Package[$ruby_pkgs],
   }
 
+  package { 'bundler':
+    ensure   => latest,
+    provider => gem,
+    require  => Package[$ruby_pkgs],
+  }
+
+
   $build_utils = [ 'build-essential', 'libpcre3-dev', 'unzip', 'libxml2-dev', 'libxslt1-dev', 'libgd2-xpm-dev', 'libgeoip-dev' ]
 
   package { $build_utils:
     ensure  => latest,
     require => Package['fpm'],
+  }
+
+  $java_pkgs = [ 'tzdata-java', 'openjdk-7-jre-headless', 'openjdk-7-jre', 'openjdk-7-jdk' ]
+
+  package { $java_pkgs:
+    ensure  => latest,
   }
 
 }
